f0ce9603cf13e45356138d9a562eabf14accb62a
tinld <19522348@gm.uit.edu.vn>
2022-11-01 08:31:10 +0700

    upload file backend

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+//---------------------------> Import <-----------------------//
+const bodyParser = require("body-parser")
 const express = require("express");
-const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const signup = require("./routers/Login.js");
+const register = require("./routers/Register.js")
+//---------------------------> Express <-----------------------//
 const app = express();
-const port = 3000;
+require('dotenv').config();
+app.use(bodyParser.json({limit: "30mb"}));
+app.use(bodyParser.urlencoded({extended : true, limit: "30mb"}));
+app.use('/', cors());
 
-app.use(morgan("combined"));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization ');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATH, DELETE');
+    next();
+})
 
-//routes
-app.get("/home", (req, res) => {
-  return res.send("Hello World");
-});
-//
-app.listen(port, () =>
-  console.log(`Server app listening at http://localhost:${port}`)
-);
+//-----------------------------> Mongoose <--------------------------//
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true});
+
+//------------------------------> Post <-----------------------------//
+app.use(signup);
+//---------------------------> Set up PORT <-------------------------//
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3001;
+}
+
+//---------------------------> ListenPort <--------------------------//
+app.listen(port, () => {
+    console.log("Server is running on port 3001");
+})
