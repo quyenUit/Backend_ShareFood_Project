b271e348435e63e4513ed66d23e26e46ed361a05
quyenUit <nguyenquyenboss@gmail.com>
2022-11-20 16:33:35 +0700

    create procfile

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 //---------------------------> Import <-----------------------//
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const signup = require("./routers/Login.js");
-const register = require("./routers/Register.js")
-const uploadpost = require("./routers/UploadPage.js")
-const getPost = require("./routers/PostList.js")
+const register = require("./routers/Register.js");
+const uploadpost = require("./routers/UploadPage.js");
+const getPost = require("./routers/PostList.js");
 //---------------------------> Express <-----------------------//
 const app = express();
-require('dotenv').config();
-app.use(bodyParser.json({limit: "30mb"}));
-app.use(bodyParser.urlencoded({extended : true, limit: "30mb"}));
-app.use('/', cors());
+require("dotenv").config();
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
+app.use("/", cors());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization ');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATH, DELETE');
-    next();
-})
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization "
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATH, DELETE");
+  next();
+});
 
 //-----------------------------> Mongoose <--------------------------//
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 
 //------------------------------> Post <-----------------------------//
 app.use(signup);
@@ -31,11 +34,11 @@ app.use(uploadpost);
 app.use(getPost);
 //---------------------------> Set up PORT <-------------------------//
 let port = process.env.PORT;
-if(port == null || port == ""){
+if (port == null || port == "") {
   port = 3001;
 }
 
 //---------------------------> ListenPort <--------------------------//
 app.listen(port, () => {
-    console.log("Server is running on port 3001");
-})
+  console.log("Server is running on port 3001");
+});
--- models/user.js
+++ models/user.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const UserDBSchema = new mongoose.Schema({
-    fname: String,
-    lname: String,
-    birthday: Date,
-    email: String,
-    password: String
-})
+  fname: String,
+  lname: String,
+  birthday: Date,
+  email: String,
+  password: String,
+});
 
-const users = mongoose.model('users', UserDBSchema);
+const users = mongoose.model("users", UserDBSchema);
 
 module.exports = users;
