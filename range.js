4e5b99d6d8be942217599459bd7271e9a2058d2b
quyenUit <nguyenquyenboss@gmail.com>
2022-12-14 11:15:29 +0700

    deploy on vercel

--- range.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-  res.header("Access-Control-Expose-Headers", "Content-Range");
-  res.header("Content-Range", "post 0-24/319");
-  res.header("Content-Range", "users/list 0-24/319");
-
-  next();
-};
