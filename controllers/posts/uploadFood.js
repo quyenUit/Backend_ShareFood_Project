dc48822cb2410d92c2a7ea78df34286bebd095f6
tinld <19522348@gm.uit.edu.vn>
2022-12-22 22:02:06 +0700

    update hour

--- controllers/posts/uploadFood.js
+++ controllers/posts/uploadFood.js
@@ -3,8 +3,8 @@ const cloudinary = require("../../utils/cloudinary.js");
 const uploadFood = async(req, res) => {
     const now = Date.now();
     let date_ob = new Date(now);
-    let start = new Date(date_ob.getFullYear(),date_ob.getMonth(),date_ob.getDate()+1, req.body.dateStart.split(':')[0]-7, req.body.dateStart.split(':')[1]);
-    let end = new Date(date_ob.getFullYear(),date_ob.getMonth(),date_ob.getDate()+1, req.body.dateEnd.split(':')[0]-7, req.body.dateEnd.split(':')[1]);
+    let start = new Date(date_ob.getFullYear(),date_ob.getMonth(),date_ob.getDate()+1, req.body.dateStart.split(':')[0], req.body.dateStart.split(':')[1]);
+    let end = new Date(date_ob.getFullYear(),date_ob.getMonth(),date_ob.getDate()+1, req.body.dateEnd.split(':')[0], req.body.dateEnd.split(':')[1]);
     const {name, type, location, file, email, amount} = req.body;
     try{
         const result = await cloudinary.uploader.upload(file, {})
